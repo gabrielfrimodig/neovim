7ba51a36881a8fc91ba765b78490313cbb5db254
Gabriel Frimodig <gabrielfrimodig@gmail.com>
2022-10-01 12:16:45 +0200

    added telescope

--- lua/maps.lua
+++ lua/maps.lua
@@ -8,3 +8,10 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", {silent = true})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {silent = true})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {silent = true})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {silent = true})
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
