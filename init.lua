1132259b7805af2ca4d25442a4ef1b70e482b80b
gabrielfrimodig <gabrielfrimodig@gmail.com>
2023-11-21 14:46:38 +0100

    adding notify background

--- init.lua
+++ init.lua
@@ -14,4 +14,7 @@ require'colorizer'.setup()
 require('plugins/cmp_config');
 require('mason').setup()
 require('plugins/mason_lspconfig')
+require("notify").setup({
+  background_colour = "#000000",
+})
 
